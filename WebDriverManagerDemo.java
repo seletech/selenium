02a5f1ba1fe420a9bee2dc619331f189c5f91202
seletech <100984028+seletech@users.noreply.github.com>
2022-04-11 23:17:03 +0530

    Update WebDriverManagerDemo.java

--- WebDriverManagerDemo.java
+++ WebDriverManagerDemo.java
@@ -1,1 +1,29 @@
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManagerDemo {
+
+	public static void main(String[] args) {
+		
+    
+    	
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		/*
+    WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+    
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+    
+    WebDriverManager.safaridriver().setup();
+		WebDriver driver = new SafariDriver(); */
+    
+	}
+
+}
